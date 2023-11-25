0ac8d13b95cc17a9d2b72b13a4a3928b30934761
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-25 14:14:02 +0500

    update projects section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /**typing animation */
 var typed = new Typed(".typing", {
-    strings:["", "Web Desinger", "Web Developer", "Responsive Web Designer", "Creative Writer", "Freelancer"],
+    strings:["", "Web Desinger", "Web Developer", "Responsive Web Designer", "Frontend Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true 
 })
+
+// , "Creative Writer", "Freelancer"
